d4be28c136741445d33ec65aa61702fce1a030de
Yuriy Ivanov <ymivanov@icloud.com>
2017-03-13 01:20:00 -0400

    added basic layout for the tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import './stylesheets/App.css';
 import * as firebase from 'firebase';
 
-class App extends Component {
+class App extends React.Component {
 
   render() {
     return (
-      <div>Testing Output</div>
+      <div>
+        <div className="panel panel-default task-editor">
+          <div className="panel-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="task">Please enter a task</label>
+                <textarea className="form-control task-input" rows="3" placeholder="I will complete..."></textarea>
+                <button className="btn btn-success task-button">Go!</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panel panel-default task-body">
+          <h3>This is a task</h3>
+          <p>This is a task description</p>
+        </div>
+      </div>
     );
   }
 }
